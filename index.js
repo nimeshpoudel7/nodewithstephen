60bca788dfa5ef6c3e53e0741b439bcd95907ff1
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-07-02 17:19:42 +0545

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express =require('express');
 const app=express();
 // app listen incomimg request
 
-app.get('/greet', (req,res)=>{
+app.get('/', (req,res)=>{
   res.send({hi:'there'})
 } )
 const PORT = process.env.PORT || 5000;
