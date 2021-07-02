432c545db2ec0cdc36b12065d9e2225c8a2c43b2
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-07-02 17:30:30 +0545

    heyChange

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express();
 // app listen incomimg request
 
 app.get('/', (req,res)=>{
-  res.send({hi:'there'})
+  res.send({hey:'bye'})
 } )
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
